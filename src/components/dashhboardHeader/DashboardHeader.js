d20129be484a91adf2e15e4b5b2d9d0821a2140c
murdzheff <murdzheff@tuta.io>
2023-04-12 14:13:58 +0300

    fixed the server, starting to build the dashboard

---
+++ src/components/dashhboardHeader/DashboardHeader.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function DashboardHeader(user) {
+  console.log(user)
+  return (
+    <div>Dashboard-header</div>
+  )
+}
+
+export default DashboardHeader
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import DashboardHeader from '../components/dashhboardHeader/DashboardHeader'
+import userManager from '../model/userManager'
 
 function Dashboard() {
+  const userId = JSON.parse(localStorage.getItem("token").userId || null)
+  const [user,setUser] = useState(null)
+
+  useEffect( () => {
+    const loggedUser = userManager.getUserById(userId).then(result => {
+      
+      setUser(result)
+    }) 
+    
+  },[])
+
+
   return (
-    <div></div>
+    <div>
+      <div className='leftSide-dashboard'>
+          <DashboardHeader user={user}/>
+      </div>
+      <div>
+
+      </div>
+    </div>
   )
 }
 
-export default Dashboard;
+export default Dashboard
