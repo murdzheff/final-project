d6ffc9c9fe7e890a3017ad83c4b270873da2aa88
murdzheff <murdzheff@tuta.io>
2023-04-16 04:30:57 +0300

    8 hours and a lot of tears, chat fixed, matches working, some server fixes and logout button added

--- src/components/leftside-dashboard/LeftsideDashboard.js
+++ src/components/leftside-dashboard/LeftsideDashboard.js
@@ -1,40 +1,88 @@
 
 import MatchesDisplay from './MatchesDisplay'
-import { useState , useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import DashboardHeader from '../dashhboardHeader/DashboardHeader'
 import userManager from '../../model/userManager'
+import "./leftSideDashboard.scss"
+import messageManager from '../../model/messageManager'
 
 
-const LeftSideContainer = ({ user,toggleModal,matches,setMatches }) => {
-    const [ clickedUser, setClickedUser ] = useState(null)
-    useEffect(()=>{
-        userManager.getUserById(JSON.parse(localStorage.getItem('token')).userId).then(res=>{
-        userManager.getUsersByIds(res.matches.map(e=>e=e.user_id)).then(result=>{
-            return result.filter(e=>{
-                return e.matches?.find(e=>e!==JSON.parse(localStorage.getItem('token')).userId)
-              })
+const LeftSideContainer = ({ toggleModal, setChats, setMatches, setRec, setType }) => {
+    const [matchedUsers, setMatchedUsers] = useState([])
+    const [currentUser,setCurrentUser] = useState(null)
+
+
+    useEffect(() => {
+        userManager.getUserById(JSON.parse(localStorage.getItem('token')).userId).then(res => {
+            
+            setCurrentUser(res);
+
+            Promise.all(res.matches.map(e => {
+
+                return e = userManager.getUserById(e.user_id)
+            })).then(result => {
+
+                let trueMatches = []
+                result.forEach(e => {
+                    if (e.matches && e.matches.find(el => el.user_id === res.user_id)) {
+                        trueMatches.push(e)
+                        setMatches(trueMatches)
+                    }
+                })
+                
+                setMatchedUsers(trueMatches)
+            })
+
 
             
+
+
+            // userManager.getUsersByIds(res.matches.map(e => e = e.user_id)).then(result => {
+            //     console.log(result)
+            //     let matched = result.filter(e => {
+            //         return e.matches?.find(e => e.user_id !== JSON.parse(localStorage.getItem('token')).userId)
+            //     })
+
+            //     setMatches(matched)
+            //     return matched;
+
+
+            // })
         })
-        })
-    })
+    }, [])
 
     return (
-        
-        
-        <div className="left-side-container">
-        <DashboardHeader/>
 
-            <div>
+
+        <div className="left-side-container">
+            {currentUser !== null ? <DashboardHeader user={currentUser} /> : null}
+
+            <div className='matches-header'>
                 <button className="option" onClick={toggleModal} >Matches</button>
-                <button className="option" onClick={toggleModal} >Chat</button>
             </div>
             <div>
-                
+                {matchedUsers.map((el,index) => {
+
+                    return el = <div className='match' key={index}>
+                            <label htmlFor={"my-button" + index}>
+                                <img className='userPhoto' src={el.photos[0]}></img>
+                                <p>{el.first_name}</p>
+                            </label>
+                            <button id={"my-button" + index} value={el.user_id} style={{ display: "none" }} onClick={(e) => {
+                                
+                                setRec(e.target.value)
+                                setChats(e.target.value)
+                                
+                                setType("Chat")
+
+
+                            }}></button>
+                        </div>
+                })}
             </div>
         </div>
-        
-        
+
+
     )
 }
 
