1e66e1e329651f366a985c34b3e08b7285a4dd02
murdzheff <murdzheff@tuta.io>
2023-04-20 20:21:12 +0300

    some more hotfixes and to pull

--- src/components/clientReviews/clientReviews.js
+++ src/components/clientReviews/clientReviews.js
@@ -20,7 +20,7 @@ export default function ClientReviews() {
         <div className="keen-slider__slide number-slide1">
 
           <div className="card testimonial-card">
-            <div className="card-up" style={{ backgroundColor: "rgb(253,54,117);" }}></div>
+            <div className="card-up" style={{ backgroundColor: "rgb(253,54,117)" }}></div>
             <div className="avatar mx-auto bg-white">
               <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(6).webp"
                 className="rounded-circle img-fluid" />
@@ -40,7 +40,7 @@ export default function ClientReviews() {
         <div className="keen-slider__slide number-slide2">
 
           <div className="card testimonial-card">
-            <div className="card-up" style={{ backgroundColor: "rgb(253,54,117);" }}></div>
+            <div className="card-up" style={{ backgroundColor: "rgb(253,54,117)" }}></div>
             <div className="avatar mx-auto bg-white">
               <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(5).webp"
                 className="rounded-circle img-fluid" />
@@ -59,7 +59,7 @@ export default function ClientReviews() {
         <div className="keen-slider__slide number-slide3">
 
           <div className="card testimonial-card">
-            <div className="card-up" style={{ backgroundColor: "rgb(253,54,117);" }}></div>
+            <div className="card-up" style={{ backgroundColor: "rgb(253,54,117)" }}></div>
             <div className="avatar mx-auto bg-white">
               <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(4).webp"
                 className="rounded-circle img-fluid" />
@@ -80,7 +80,7 @@ export default function ClientReviews() {
         <div className="keen-slider__slide number-slide4">
 
           <div className="card testimonial-card">
-            <div className="card-up" style={{ backgroundColor: "rgb(253,54,117);" }}></div>
+            <div className="card-up" style={{ backgroundColor: "rgb(253,54,117)" }}></div>
             <div className="avatar mx-auto bg-white">
               <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(3).webp"
                 className="rounded-circle img-fluid" />
@@ -99,7 +99,7 @@ export default function ClientReviews() {
         <div className="keen-slider__slide number-slide5">
 
           <div className="card testimonial-card">
-            <div className="card-up" style={{ backgroundColor: "rgb(253,54,117);" }}></div>
+            <div className="card-up" style={{ backgroundColor: "rgb(253,54,117)" }}></div>
             <div className="avatar mx-auto bg-white">
               <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(2).webp"
                 className="rounded-circle img-fluid" />
@@ -118,7 +118,7 @@ export default function ClientReviews() {
         <div className="keen-slider__slide number-slide6">
 
           <div className="card testimonial-card">
-            <div className="card-up" style={{ backgroundColor: "rgb(253,54,117);" }}></div>
+            <div className="card-up" style={{ backgroundColor: "rgb(253,54,117)" }}></div>
             <div className="avatar mx-auto bg-white">
               <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(9).webp"
                 className="rounded-circle img-fluid" />
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -4,37 +4,38 @@ import LeftSideContainer from '../components/leftside-dashboard/LeftsideDashboar
 // import userManager from '../model/userManager'
 import CardsContainer from '../components/cards/CardsContainer'
 import Chat from '../components/chat/Chat'
-import { useNavigate } from "react-router-dom"
 import messageManager from '../model/messageManager'
 import MoreInfo from '../components/moreInfo/MoreInfo'
+import React from 'react'
 
 
 
 
 
 function Dashboard(props) {
-
   const [type, setType] = useState("Matches")
   const [matches, setMatches] = useState([])
   const [rec, setRec] = useState(null)
-  const [chats,setChats] = useState([])
-  const [infoUser,setInfoUser] = useState(rec)
+  const [chats, setChats] = useState([])
+  const [infoUser, setInfoUser] = useState(rec)
+
+
 
   function sortByTimestamp(objects) {
     return objects.sort((a, b) => new Date(a.timestamp) - new Date(b.timestamp));
-}
+  }
 
 
 
-async function update(id) {
+  async function update(id) {
     await messageManager.getMessages(props.loggedUser.user_id, id).then(mess => {
-        let sorted = sortByTimestamp(mess)
-        setChats(sorted)
+      let sorted = sortByTimestamp(mess)
+      setChats(sorted)
     })
-}
+  }
 
   const toggleModal = (e) => {
-    
+
     e.preventDefault();
     if (e.target.textContent === "Matches") {
       setType("Matches")
@@ -46,10 +47,10 @@ async function update(id) {
   const token = localStorage.getItem("token");
   const user = token ? JSON.parse(token).userId : null;
 
-  
+
 
   if (user === null) {
-    
+
     return null
   }
 
@@ -57,6 +58,9 @@ async function update(id) {
 
   return (
     <div className='dashboard'>
+
+
+
       {props.loggedUser?.photos && <LeftSideContainer
         type={type}
         matches={matches}
@@ -66,31 +70,31 @@ async function update(id) {
         setRec={setRec}
         setType={setType}
         setChats={update} />}
-        
-      
-      {type === "Matches" && props.loggedUser && 
-      <CardsContainer 
-      update={props.update} 
-      setUpdate={props.setUpdate} 
-      matches={matches} 
-      loggedUser={props.loggedUser} 
-      setInfoUser={setInfoUser} 
-      setType={setType} 
-      setMatches={setMatches} 
-      type={type}></CardsContainer>}
-      {rec !== null && type === "Chat" ? 
-      <Chat 
-      loggedUser={props.loggedUser} 
-      setInfoUser={setInfoUser} 
-      setType={setType} 
-      correspondingUserId={rec} 
-      type={type}></Chat> : null}
-      {rec !== null && 
-      type === "info" ? 
-      <MoreInfo 
-      type={type} 
-      loggedUser={props.loggedUser} user={infoUser} ></MoreInfo> : null}
-      
+
+
+      {type === "Matches" && props.loggedUser &&
+        <CardsContainer
+          update={props.update}
+          setUpdate={props.setUpdate}
+          matches={matches}
+          loggedUser={props.loggedUser}
+          setInfoUser={setInfoUser}
+          setType={setType}
+          setMatches={setMatches}
+          type={type}></CardsContainer>}
+      {rec !== null && type === "Chat" ?
+        <Chat
+          loggedUser={props.loggedUser}
+          setInfoUser={setInfoUser}
+          setType={setType}
+          correspondingUserId={rec}
+          type={type}></Chat> : null}
+      {rec !== null &&
+        type === "info" ?
+        <MoreInfo
+          type={type}
+          loggedUser={props.loggedUser} user={infoUser} ></MoreInfo> : null}
+
 
 
     </div>
