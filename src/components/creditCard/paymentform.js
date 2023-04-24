51f348a95e93133b5c8b2fa4232019ad23cfcd9b
AnzheloStoyanov <anjelostoqnov@gmail.com>
2023-04-24 22:51:14 +0300

    added css

--- src/components/creditCard/paymentform.js
+++ src/components/creditCard/paymentform.js
@@ -1,113 +1,136 @@
+import React, { useState } from "react";
+import {
+  CardNumberElement,
+  CardExpiryElement,
+  CardCvcElement,
+  useElements,
+  useStripe,
+} from "@stripe/react-stripe-js";
+import axios from "axios";
+import "./CreditCardForm.css";
 
-import React, { Suspense, useState } from "react";
-import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js'
-import axios from 'axios'
-import './CreditCardForm.css'
-
-const CARD_OPTIONS = {
-    iconStyle: "solid",
-    style: {
-        base: {
-            iconColor: "#c4f0ff",
-            color: "#fff",
-            fontWeight: 500,
-            fontFamily: "Roboto, Open Sans, Segoe UI, sans-serif",
-            fontSize: "16px",
-            fontSmoothing: "antialiased",
-            ":-webkit-autofill": { color: "#fce883" },
-            "::placeholder": { color: "#87bbfd" }
-        },
-        invalid: {
-            iconColor: "#ffc7ee",
-            color: "#ffc7ee"
-        }
-    }
-}
+// Define shared card element options
+const CARD_ELEMENT_OPTIONS = {
+  style: {
+    base: {
+      color: "#545454",
+      fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+      fontSmoothing: "antialiased",
+      fontSize: "16px",
+      "::placeholder": {
+        color: "#aab7c4",
+      },
+    },
+    invalid: {
+      color: "#fa755a",
+      iconColor: "#fa755a",
+    },
+  },
+};
 
 export default function PaymentForm(props) {
-    const [success, setSuccses] = useState(false)
-    const stripe = useStripe()
-    const elements = useElements()
-    const [modal, setModal] = useState(true)
-console.log(props)
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        const { error, paymentMethod } = await stripe.createPaymentMethod({
-            type: "card",
-            card: elements.getElement(CardElement)
-        })
+  const [success, setSuccess] = useState(false);
+  const stripe = useStripe();
+  const elements = useElements();
+  const [modal, setModal] = useState(true);
+  console.log(props);
 
-        if (!error) {
-            try {
-                const { id } = paymentMethod
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card: elements.getElement(CardNumberElement),
+    });
 
-                const response = await axios.post("http://localhost:8080/payment", {
-                    amount: 1000,
-                    id
-                })
-                if (response.data.success) {
-                    axios.put('http://localhost:8080/user/' + JSON.parse(localStorage.getItem("token")).userId + '/paymentstatus', {
-                        paymentStatus: true
-                    })
-                        .then(function (response) {
-                            console.log("Sussessfull payment")
-                            setSuccses(true)
-                            props.loggedUser.paymentStatus=true;
+    if (!error) {
+      try {
+        const { id } = paymentMethod;
 
-                        })
-                        .catch(function (error) {
-                            console.error(error);
-                        });
-
-
-                }
-            } catch (error) {
-                console.log("Error", error)
-
-            }
-
+        const response = await axios.post("http://localhost:8080/payment", {
+          amount: 1000,
+          id,
+        });
+        if (response.data.success) {
+          axios
+            .put(
+              "http://localhost:8080/user/" +
+                JSON.parse(localStorage.getItem("token")).userId +
+                "/paymentstatus",
+              {
+                paymentStatus: true,
+              }
+            )
+            .then(function (response) {
+              console.log("Successful payment");
+              setSuccess(true);
+              props.loggedUser.paymentStatus = true;
+            })
+            .catch(function (error) {
+              console.error(error);
+            });
         }
-        else {
-            console.log(error.message)
-
-        }
+      } catch (error) {
+        console.log("Error", error);
+      }
+    } else {
+      console.log(error.message);
     }
-    if(!modal){
-        return null
-    }
-    return (
-        <>
-            {!success ?
-                <div className="backgroun-overlay-credit-card">
-                    <div className="payment-form-m">
-                        <div className="payment-form-text">
-                            <h2> Нямаш заредена опцията за </h2>
-                            <h2> супер харесвания! </h2>
-                            <img className="image-card" src={props.user.photos[0]}></img>
-                            <p> Не изпускай {props.user.first_name}, презареди сега!</p>
+  };
 
-                        </div>
-                        <form className="payment-form" onSubmit={handleSubmit}>
-                            <fieldset className="FormGroup">
-                                <div className="FormRow">
-                                    <CardElement options={CARD_OPTIONS} />
-                                </div>
-                            </fieldset>
-                            <button className="paymentButton">Pay</button>
-                        </form>
-                    </div>
+  if (!modal) {
+    return null;
+  }
+  return (
+    <>
+      {!success ? (
+        <div className="backgroun-overlay-credit-card">
+          <div className="payment-form-m">
+            <div className="payment-form-text">
+              <h2> Нямаш заредена опцията за </h2>
+              <h2> супер харесвания! </h2>
+              <img className="image-card" src={props.user.photos[0]}></img>
+              <p> Не изпускай {props.user.first_name}, презареди сега!</p>
+            </div>
+            <form className="payment-form" onSubmit={handleSubmit}>
+              <fieldset className="FormGroup">
+                <div className="FormRow">
+                  <label htmlFor="cardNumber">Card number</label>
+                  <CardNumberElement
+                    id="cardNumber"
+                    options={CARD_ELEMENT_OPTIONS}
+                  />
                 </div>
-                :
-                <div className="backgroun-overlay-credit-card">
-                     <div className="payment-form">
-                     <div>
-                        <h2> You just Subscribe now you can swipe Up </h2>
-                    </div>
-                    <button onClick={()=>{setModal(!modal)}} >X</button>
-                     </div>
+                <div className="FormRow">
+                  <label htmlFor="cardExpiry">Expiry date</label>
+                  <CardExpiryElement
+                    id="cardExpiry"
+                    options={CARD_ELEMENT_OPTIONS}
+                  />
                 </div>
-               
-            }
-        </>
-    )
+                <div className="FormRow">
+                  <label htmlFor="cardCvc">CVC</label>
+                  <CardCvcElement id="cardCvc" options={CARD_ELEMENT_OPTIONS} />
+                </div>
+              </fieldset>
+              <button className="paymentButton">Pay</button>
+            </form>
+          </div>
+        </div>
+      ) : (
+        <div className="backgroun-overlay-credit-card">
+          <div className="payment-form-c">
+            <div className="closeContainer">
+              <h2 className="aftaerPayText"> Честито ти взе своя абонамент, сега вече не можеш да изпуснеш {props.user.first_name} </h2>
+              <img className="image-card" src={props.user.photos[0]}></img>
+
+            </div>
+            <button className = "closeBtn" onClick={() => setModal(!modal)}>Продължи</button>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
+
+
+
